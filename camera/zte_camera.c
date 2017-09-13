18dbb6541eb44e32e400a70865407941b1c3b9e7
Bauuuuu <abansode@outlook.com>
2017-09-13 15:53:45 +0200

    nx512j: camera: update the configs add missing symbol
    Change-Id: If993ed353c4083ec569332b703191318cb7ffde8

--- camera/zte_camera.c
+++ camera/zte_camera.c
@@ -60,3 +60,4 @@ void _ZN7android13SensorManagerC1Ev(void *sensorMgr)
     /* for nubia camera stock app */
 extern void _ZN8SkBitmap9setConfigENS_6ConfigEiij11SkAlphaType() {}
 extern void _ZN8SkBitmap9setConfigENS_6ConfigEiim11SkAlphaType() {}
+extern void _ZN14SkImageDecoder10DecodeFileEPKcP8SkBitmap11SkColorTypeNS_4ModeEPNS_6FormatE() {}
