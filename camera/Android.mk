73737cbb07132b8fd35c1c130f2144e3589e9459
Flex1911 <dedsa2002@gmail.com>
2017-09-13 15:57:04 +0200

    nx512j: Bring back atomic calls for camera
    Change-Id: I529971e48bc8359fb6d960973eaa222dbe357ce3

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,9 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 #Include the ZTE camera Symbols
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
-LOCAL_SRC_FILES := zte_camera.c
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils libsensor
+LOCAL_SRC_FILES := zte_camera.c atomic.cpp
 LOCAL_MODULE := libzte_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
