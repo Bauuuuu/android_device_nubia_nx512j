da0a11a60e3c3e85c2b33ae484d10e833059d073
LuK1337 <priv.luk@gmail.com>
2017-09-15 10:15:07 +0200

    nx512j: gps: Add flp shim
    Change-Id: Iba1343dd91518c54de797b4705cc905aa956e583

--- modules/Android.mk
+++ modules/Android.mk
@@ -1,7 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libshim_gps/get_process_name.c
+LOCAL_SRC_FILES := \
+    libshim_gps/get_process_name.c
 LOCAL_MODULE := libshim_gps
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+    flp/flp.c
+LOCAL_MODULE := libshims_flp
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
