0fd44e54435b142cb0022fc11fd2add73837e3b0
Bauuuuu <abansode@outlook.com>
2017-10-23 13:03:54 +0200

    nx513j: rebrand to lineagehw
    * cleanup the unused flags
    Change-Id: Iabc6488bdf60712014004ba7b3af8b688638c001

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,8 +1,9 @@
-# CMHW
-BOARD_HARDWARE_CLASS += device/nubia/nx513j/cmhw
-BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
-#BOARD_USES_CYANOGEN_HARDWARE := true
-#TARGET_TAP_TO_WAKE_NODE := "/data/tp/easy_wakeup_gesture"
+# lineagehw
+BOARD_HARDWARE_CLASS += \
+    $(VENDOR_PATH)/lineagehw
+
+# double tap to wake gesture
+TARGET_TAP_TO_WAKE_NODE := "/data/tp/easy_wakeup_gesture"
 
 #Real time battery charging 
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
