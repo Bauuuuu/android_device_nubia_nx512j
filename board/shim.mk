545c017d490db15ba211558075070d088c02bd42
Paul Keith <javelinanddart@gmail.com>
2018-02-15 23:33:28 +0100

    nx12j: shim: Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
     * As a bonus, we no longer need noatsecure
       to make LD_SHIM_LIBS persist through services
    
    Change-Id: I877192422062f3e59c81a7130ad1a2b4be5d1647

---
+++ board/shim.mk
@@ -0,0 +1,7 @@
+# SHIM
+TARGET_LD_SHIM_LIBS := /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_atomic.so \
+    /system/lib/hw/camera.msm8916.so|libzte_camera.so \
+    /system/vendor/lib64/libizat_core.so|libshim_gps.so \
+    /system/vendor/lib64/libizat_core.so|libshims_flp.so \
+    /system/lib/hw/camera.msm8916.so|libshim_atomic.so
