bfdcc8ae77f58bba11559e71a1160d4d338d1233
Bauuuuu <abansode@outlook.com>
2018-07-31 17:07:24 +0200

    nx512j: import nubia charger

--- product/charger.mk
+++ product/charger.mk
@@ -2,3 +2,6 @@
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Nubia charger
+include $(LOCAL_PATH)/rootdir/charger/offcharge.mk
