9fd9e7f9b7bfd135c70a53e58d6c8fcf3ee71d10
Bruno Martins <bgcngm@gmail.com>
2018-07-31 17:07:24 +0200

    nx512j: import nubia charger

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -88,3 +88,14 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+# Offmode charging
+include $(CLEAR_VARS)
+LOCAL_MODULE          := poweroffcharge
+LOCAL_MODULE_OWNER    := nubia
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/poweroffcharge
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)
